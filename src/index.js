c1e850995641d5fbc75549b7c0a4047a6bf2d808
xxxgrime <mxer12390@gmail.com>
2018-09-05 07:34:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 //import css from './file.css'
 import css from './materializecss.min.css'
-import {AppProvider,AppWrapper,Main} from './components/app.js'
-import {data} from './components/data.js'
-import {Form,ContextComponent} from './components/form.js'
-import {Navbar} from './components/navbar'
-import {BrowserRouter as Router} from 'react-router-dom' 
+import {AppProvider,Main,} from './components/app.js' 
 
 
 
